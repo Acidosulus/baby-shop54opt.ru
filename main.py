434c5d5d7868d2f2cb70bd62d172fbc85760cc48
Acidosulus <acidos@yandex.ru>
2024-05-19 21:09:36 +0700

    Sun 05/19/2024 21:09:36.34

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from bs4 import BeautifulSoup
 import tempfile
 import rich
+from my_library import append_if_not_exists
 
 async def fetch_html(url, session):
 	async with session.get(url) as response:
@@ -19,9 +20,32 @@ def get_list_of_goods_from_source(source):
 	table = BeautifulSoup(source, features='html5lib').find('article')
 	carts = table.find_all('div',{'class':'product-item inline'})
 	for cart in carts:
-		rich.print(cart.find('a'))
+		append_if_not_exists(cart.find('a').get('href'), result)
 	return result
 
+
+async def load_data_by_good_from_link(link:str, session):
+	html = await fetch_html(link, session)
+	soup = BeautifulSoup(html, features='html5lib')
+	result = {}
+	result['title'] = soup.find('h1').text
+	
+	cost = soup.find('span',{'class':'sale-price'})
+	if cost:
+		cost = cost.find('span',{'class':'product-price-data'}).get('data-cost')
+	else:
+		cost = soup.find('span',{'class':'product-price-data'}).get('data-cost')
+	result['cost'] = cost
+
+	images_container = soup.find('div', {'class':'avatar-wrap image'})
+	rich.print(images_container)
+	links = images_container.find_all('a')
+	rich.print(links)
+	result['pictures']=[]
+	for ilink in links:
+		append_if_not_exists(ilink.get('href').replace('//','https://'), result['pictures'])
+		
+	return result
 
 async def get_link_on_catalog_pages(catalog_url:str):
 	all_pages = []
@@ -42,6 +66,11 @@ async def get_link_on_catalog_pages(catalog_url:str):
 	return all_pages
 
 async def main():
+	async with aiohttp.ClientSession() as session:
+		result = await load_data_by_good_from_link('https://baby-shop54opt.ru/products/32783487', session)
+		rich.print(result)
+
+	return
 	url = 'http://example.com'
 	html = await get_link_on_catalog_pages('https://baby-shop54opt.ru/products/category/3000271')
 	print(html)
