29dcdadaf9b6927d467dc4a17a24fbbcdf00f337
Acidosulus <acidos@yandex.ru>
2024-05-20 16:48:45 +0700

    Mon 05/20/2024 16:48:45.56

--- driver.py
+++ driver.py
@@ -3,9 +3,13 @@ import rich
 import pika
 import configparser
 import threading
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 class RabbitMQ_Driver:
 	server_address = ''
+	server_port = ''
 	user_name = ''
 	user_password = ''
 	queue_prefix = ''
@@ -16,6 +20,7 @@ class RabbitMQ_Driver:
 	def __init__(self):
 		self.config.read('settings.ini')
 		self.server_address = self.config["RabbitMQ"]["adress"]
+		self.port = self.config["RabbitMQ"]["port"]
 		self.user_name = self.config["RabbitMQ"]["user"]
 		self.user_password = self.config["RabbitMQ"]["password"]
 		self.queue = self.config["RabbitMQ"]["queue"]
@@ -23,27 +28,18 @@ class RabbitMQ_Driver:
 																	  			credentials = pika.PlainCredentials(	username  = self.user_name,
 																														 password = self.user_password)))
 		self.channel = self.connection.channel()
-		rich.print(self.connection)
-		rich.print(self.channel)
+		# rich.print(self.connection)
+		# rich.print(self.channel)
 
 
-	def add_queue(self, queue_name:str, callback: callable):
-		self.channel.queue_declare(queue=queue_name)
-		self.channel.basic_consume(		queue=queue_name,
-							 			on_message_callback=callback,
-										auto_ack=False)
-		self.channel.start_consuming()
-		rich.print(f'Add queue: {queue_name}')
+	def create_queue(self, queue_name:str):
+		self.channel.queue_declare(queue=queue_name, durable=True)
+		rich.print(f'Create queue: {queue_name}')
 
 
 	def add_queue_non_blocking(self, queue_name: str, callback: callable):
-		self.channel.queue_declare(queue=queue_name)
 		self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
-		t = threading.Thread(target=self.channel.start_consuming)
-		t.daemon = True
-		t.start()
-		rich.print(f'Add queue: {queue_name}')
-
+		self.channel.start_consuming()
 
 	def put_message(self, queue_name, message):
 		rich.print(f'Put message: {message} into queue: {queue_name}')
@@ -53,18 +49,32 @@ class RabbitMQ_Driver:
 	def recieve_message(self, ch, method, properties, body:bytes):
 		rich.print(f"Received: '{body.decode(encoding='utf8',  errors='ignore')}', ch:{ch}, method:{method}, properties:{properties}")
 
+	def get_broker_queues(self):
+		response = requests.get(f'http://{self.server_address}:{self.port}/api/queues', auth=HTTPBasicAuth(self.user_name, self.user_password))
+		if response.status_code == 200:
+			queues = response.json()
+			return [queue['name'] for queue in queues]
+		else:
+			print(f"Не удалось получить список очередей. Статус код: {response.status_code}\nПричина: {response.text}")
+			return []
 
 	def __del__(self):
-		self.connection.close()
+		try:
+			self.connection.close()
+		except:
+			...
 
 
 def my_test_callback(ch, method, properties, body:bytes):
+	rich.print('consume:')
 	rich.print(f"From:my_test_callback Received: '{body.decode(encoding='utf8',  errors='ignore')}', ch:{ch}, method:{method}, properties:{properties}")
-	ch.basic_ack(delivery_tag=method.delivery_tag)
+	# ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 if __name__ == '__main__':
-	rd = RabbitMQ_Driver()
-	rd.add_queue_non_blocking('test', my_test_callback)
-	rd.put_message('test', 'test_message_1')
+	...
+	# rd = RabbitMQ_Driver()
+	# for i in range(10): rd.put_message('test', f'test_message_{i}')
+	# rd.add_queue_non_blocking('test', my_test_callback)
+	
 
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import aiohttp
-import asyncio
+import requests
 from bs4 import BeautifulSoup
 import tempfile
+import datetime
 import rich
+import sys
+import json
 from my_library import append_if_not_exists, clear_spaces, Price, prepare_str, prepare_for_csv_non_list
+from driver import RabbitMQ_Driver
 
-async def fetch_html(url, session):
-	async with session.get(url) as response:
-		return await response.text()
+
+def download_page(url):
+    # Отправляем GET запрос по указанному URL
+    response = requests.get(url)
+   
+    # Проверяем статус ответа
+    if response.status_code == 200:
+        # Если статус ответа успешный (200), возвращаем код страницы
+        return response.text
+    else:
+        # Если статус ответа не успешный, выдаем ошибку
+        print("Ошибка при загрузке страницы. Код ответа:", response.status_code)
+        return None
 
 
 def save_temp_file(data):
@@ -24,8 +37,8 @@ def get_list_of_goods_from_source(source):
 	return result
 
 
-async def load_data_by_good_from_link(link:str, session):
-	html = await fetch_html(link, session)
+def load_data_by_good_from_link(link:str, session):
+	html = download_page(link)
 	soup = BeautifulSoup(html, features='html5lib')
 	result = {}
 
@@ -50,58 +63,60 @@ async def load_data_by_good_from_link(link:str, session):
 
 	return result
 
-async def get_all_links_on_goods(catalog_url:str):
+def get_all_links_on_goods(catalog_url:str, queue:str, price_path:str, rqd:RabbitMQ_Driver):
+	links_queue_name = queue+'_links_on_goods'
 	all_pages = []
 	links = []
 	page_number =1
 	html, html_last = '',''
-	async with aiohttp.ClientSession() as session:
-		while True:
-			rich.print(f'catalog page: {page_number}')
-			next_page_url = f"{catalog_url}?page={page_number}"
-			html_last = html
-			html = await fetch_html(next_page_url, session)
-			if str(BeautifulSoup(html, features='html5lib').find('article')) != str(BeautifulSoup(html_last, features='html5lib').find('article')):
-				all_pages.append(next_page_url)
-				result = get_list_of_goods_from_source(html)
-				links_from_page = get_list_of_goods_from_source(html)
-				rich.print(links_from_page)
-				links.extend(links_from_page)
-				page_number += 1
-			else:
-				break
+	while True:
+		rich.print(f'catalog page: {page_number}')
+		next_page_url = f"{catalog_url}?page={page_number}"
+		html_last = html
+		html = download_page(next_page_url)
+		if str(BeautifulSoup(html, features='html5lib').find('article')) != str(BeautifulSoup(html_last, features='html5lib').find('article')):
+			all_pages.append(next_page_url)
+			result = get_list_of_goods_from_source(html)
+			links_from_page = get_list_of_goods_from_source(html)
+			rich.print(links_from_page)
+			links.extend(links_from_page)
+			rqd.create_queue(queue+'_links_on_goods')
+			for lnk in links_from_page:
+				rqd.put_message(links_queue_name, json.dumps({'price':price_path, 'catalog_url':catalog_url, 'link':lnk}))
+			page_number += 1
+		else:
+			break
 	return links
 
 
-async def load_data_by_good_from_link(link:str, session, semaphore):
-	async with semaphore:
-		html = await fetch_html(link, session)
-		soup = BeautifulSoup(html, features='html5lib')
-		result = {}
+def load_data_by_good_from_link(link:str):
+	html = download_page(link)
+	soup = BeautifulSoup(html, features='html5lib')
+	result = {}
 
-		result['link'] = link
+	result['link'] = link
 
-		result['title'] = soup.find('h1').text
-		
-		cost = soup.find('span',{'class':'sale-price'})
-		if cost:
-			cost = cost.find('span',{'class':'product-price-data'}).get('data-cost')
-		else:
-			cost = soup.find('span',{'class':'product-price-data'}).get('data-cost')
-		result['cost'] = cost
+	result['title'] = soup.find('h1').text
+	
+	cost = soup.find('span',{'class':'sale-price'})
+	if cost:
+		cost = cost.find('span',{'class':'product-price-data'}).get('data-cost')
+	else:
+		cost = soup.find('span',{'class':'product-price-data'}).get('data-cost')
+	result['cost'] = cost
 
-		images_container = soup.find('div', {'class':'avatar-wrap image'})
-		links = images_container.find_all('a')
-		result['pictures']=[]
-		for ilink in links:
-			append_if_not_exists(ilink.get('href').replace('//','https://'), result['pictures'])
+	images_container = soup.find('div', {'class':'avatar-wrap image'})
+	links = images_container.find_all('a')
+	result['pictures']=[]
+	for ilink in links:
+		append_if_not_exists(ilink.get('href').replace('//','https://'), result['pictures'])
 
-		result['description'] = clear_spaces(soup.find('div',{'class':'ui tab segment active'}).text.replace('\n', ' ')).strip()
+	result['description'] = clear_spaces(soup.find('div',{'class':'ui tab segment active'}).text.replace('\n', ' ')).strip()
 
-		result['sizes']= ''
-		
-		rich.print(result)
-		return result
+	result['sizes']= ''
+	
+	rich.print(result)
+	return result
 
 def save_price(goods:list, path:str):
 	price = Price(path)
@@ -117,31 +132,51 @@ def save_price(goods:list, path:str):
 		price.write_to_csv(path)
 
 
+queue_name = "baby-shop54opt.ru" + datetime.datetime.now().strftime("%Y-%m-%d")
 
-async def main():
-	# goods = []
-	# good_links = ['https://baby-shop54opt.ru/products/58284052',]
+rqd = RabbitMQ_Driver()
 
 
-	# semaphore = asyncio.Semaphore(20)
-	# async with aiohttp.ClientSession() as session:
-	# 	tasks = [load_data_by_good_from_link(link, session, semaphore) for link in good_links]
-	# 	results = await asyncio.gather(*tasks)
-	# 	save_price(results, 'G:\baby-shop54opt.ru\csvs\test.csv')
+def consume_link_on_good(ch, method, properties, body:bytes):
+	# rich.print(ch)
+	# rich.print(method.routing_key)
+	# rich.print(properties)
+	data = json.loads(body.decode(encoding='utf8',  errors='ignore'))
+	rich.print(f"""Recieved link: {data['link']} from catalog: {data['catalog_url']} for price: {data['price']}""")
+	method_frame, header_frame, body = ch.basic_get(queue=method.routing_key)
+	good_data = load_data_by_good_from_link(data['link'])
+	rqd.create_queue(method.routing_key+'_good')
+	rqd.put_message(method.routing_key+'_good', json.dumps(good_data, ensure_ascii=False))
+	# if method_frame is None:
+	# 	ch.stop_consuming()
+	# 	rich.print('[red]Stop consuming[/red]')
 	
 
 
-	# result = await load_data_by_good_from_link('https://baby-shop54opt.ru/products/32783487', session)
-	# goods.append(result)
-	# rich.print(result)
+def main():
+	if sys.argv[1]=='get_links_from_catalog':
+		# liks_on_goods = await get_all_links_on_goods('https://baby-shop54opt.ru/products/category/3000271')
+		get_all_links_on_goods(	
+								catalog_url='https://baby-shop54opt.ru/products/category/3000271',
+					   			queue=queue_name,
+								price_path=r'G:\baby-shop54opt.ru\csvs\test.csv',
+								rqd=rqd
+								)
 
-	
-	liks_on_goods = await get_all_links_on_goods('https://baby-shop54opt.ru/products/category/3000271')
+	if sys.argv[1]=='get_goods_data':
+		queues = rqd.get_broker_queues()
+		queues = list(filter(lambda x: x.startswith('baby-shop54opt.ru') and x.endswith('_links_on_goods'), queues))
 
-	semaphore = asyncio.Semaphore(20)
-	async with aiohttp.ClientSession() as session:
-		tasks = [load_data_by_good_from_link(link, session, semaphore) for link in liks_on_goods]
-		results = await asyncio.gather(*tasks)
+		for queue in queues:
+			rqd.add_queue_non_blocking(queue, consume_link_on_good)
+
+		# semaphore = asyncio.Semaphore(20)
+		# async with aiohttp.ClientSession() as session:
+		# 	tasks = [load_data_by_good_from_link(link, session, semaphore) for link in links_on_goods]
+		# 	results = await asyncio.gather(*tasks)
+
+	if sys.argv[1]=='unload_prices':
 		save_price(results, r'G:\baby-shop54opt.ru\csvs\test.csv')
 
-asyncio.run(main())
+if __name__ == '__main__':
+	main()
